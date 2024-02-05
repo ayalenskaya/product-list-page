33d6c0c1d658e8cb16d448cfe60700c7e26f6c90
ayalenskaya <kamio-vk@yandex.ru>
2024-02-05 19:09:27 +0300

    GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
+  base: '/product-list-page/',  
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), 
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist',
+  },
 });
-
- 
